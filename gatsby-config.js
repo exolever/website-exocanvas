9b7a19eb8ac67699f8dfdbacb6a8aa150e588482
Ernesto Serrano <info@ernesto.es>
2019-07-03 09:53:59 +0200

    Added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,23 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-tagmanager`,
       options: { id: "GTM-MGZKDPL" }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-135475643-6",
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "0",
+        cookieDomain: "exocanvas.com",
+      },
+    },
   ]
 }
